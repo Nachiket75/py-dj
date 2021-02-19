763ca6322b215076005f26481e2f55605299b167
Nachiket75 <nachinatekar@gmail.com>
2021-02-19 17:00:05 +0530

    added html file and showing dynamic cotents using ginger template to access the name set in views.py using {{name}} this is ginger pattern to get dynamic data you can also perfrom mathematical operation.This  is define in home.html

--- DemoWebapp/views.py
+++ DemoWebapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def courses(request):
-    return  HttpResponse('<h1>This is my Home page</h1')
+    #return  HttpResponse('<h1>This is my Home page</h1')
+    return  render(request,'home.html', {'name':'Nachiket'})
